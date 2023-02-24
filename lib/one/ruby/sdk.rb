4a92419488628679c794a469008e92461dfdb94f
Ben Kittrell <ben@doodlekit.com>
2023-02-23 20:55:51 -0600

    Initial commit

---
+++ lib/one/ruby/sdk.rb
@@ -0,0 +1,10 @@
+require "one/ruby/sdk/version"
+
+module One
+  module Ruby
+    module Sdk
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/one/ruby/sdk_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe One::Ruby::Sdk do
+  it "has a version number" do
+    expect(One::Ruby::Sdk::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
